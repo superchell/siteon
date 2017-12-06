c6e1335f18cad094173c2e922afc307822b4bfbf
superchel <for.oleg.mozolev@gmail.com>
2017-12-06 17:06:51 +0200

    added smtp configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,15 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "siteon_#{Rails.env}"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.zoho.com',
+    port:                 587,
+    domain:               'siteon.com.ua',
+    user_name:            'info@siteon.com.ua',
+    password:             '2TW9eswigL',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
