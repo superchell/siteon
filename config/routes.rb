ea2deaec5474543e2432e8b9f6e35c227b58e970
superchel <for.oleg.mozolev@gmail.com>
2017-12-07 12:21:32 +0200

    added redirects from home and reviews post types

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  scope PluginRoutes.system_info["relative_url_root"], as: "cama" do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get "home/:id", to: redirect("/")
+    get "home", to: redirect("/")
+
+    get "reviews/:id", to: redirect("/")
+    get "reviews", to: redirect("/")
+  end
 end
