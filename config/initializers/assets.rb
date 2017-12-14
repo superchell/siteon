2634144ba05fc52692ebd98c7aab98ce75820163
superchel <for.oleg.mozolev@gmail.com>
2017-12-14 12:37:25 +0200

    changed assets config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,12 +7,12 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w( apps/themes/site_on/assets/* )
+Rails.application.config.assets.precompile += %w( themes/site_on/assets/* )
 Rails.application.config.assets.precompile += %w( tinymce-jquery.js )
 Rails.application.config.assets.precompile += %w( lib/jquery.min.js )
 
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
-Rails.application.config.assets.precompile += %w(.svg .eot .woff .ttf)
+Rails.application.config.assets.precompile += %w(.svg .eot .woff .ttf .js .css)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
