8daacdbeba982375d305c9f9eecf694e840a2c77
superchel <for.oleg.mozolev@gmail.com>
2017-12-15 12:04:47 +0200

    changed production config settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.public_file_server.headers = {
-      'Cache-Control' => 'public, s-maxage=31536000, maxage=15552000',
+      'Cache-Control' => 'public, s-maxage=31536000, max-age=15552000',
       'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
   }
 
@@ -27,14 +27,15 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true #ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
