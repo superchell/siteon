c4bd81e5f66db86aab8613569fac0d4f309b2d01
superchel <for.oleg.mozolev@gmail.com>
2017-12-21 14:31:51 +0200

    added added http options to redirect match

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   constraints(host: /^www\./i) do
     match '(*any)' => redirect { |params, request|
       URI.parse(request.url).tap { |uri| uri.host.sub!(/^www\./i, '') }.to_s
-    }
+    }, via: [:get, :post]
   end
 
   scope PluginRoutes.system_info["relative_url_root"], as: "cama" do
