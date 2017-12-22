3c03f1348331bd4b83b8d2e48cbb8926b983e644
superchel <for.oleg.mozolev@gmail.com>
2017-12-22 16:50:41 +0200

    experiment with precompile 6

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,13 +6,12 @@ Rails.application.config.assets.version = '1.1'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-# Rails.application.config.assets.paths << Rails.root.join('node_modules')
-# Rails.application.config.assets.precompile += %w( themes/site_on/assets/* )
-# Rails.application.config.assets.precompile += %w( tinymce-jquery.js )
-# Rails.application.config.assets.precompile += %w( lib/jquery.min.js )
-#
-# Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
-# Rails.application.config.assets.precompile += %w(.svg .eot .woff .ttf .js .css)
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts", "apps")
+Rails.application.config.assets.precompile += %w( themes/site_on/assets/* )
+Rails.application.config.assets.precompile += %w( tinymce-jquery.js )
+Rails.application.config.assets.precompile += %w( lib/jquery.min.js )
+Rails.application.config.assets.precompile += %w(.svg .eot .woff .ttf .js .css)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
