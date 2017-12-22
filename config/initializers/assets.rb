520fc6ec7160ce0bce8eef837348d788e84a4989
superchel <for.oleg.mozolev@gmail.com>
2017-12-22 16:23:24 +0200

    experiment with precompile 4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,7 @@ Rails.application.config.assets.precompile += %w( tinymce-jquery.js )
 Rails.application.config.assets.precompile += %w( lib/jquery.min.js )
 
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
-Rails.application.config.assets.precompile += %w(.svg .eot .woff .ttf .js .css .scss)
+Rails.application.config.assets.precompile += %w(.svg .eot .woff .ttf .js .css)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
