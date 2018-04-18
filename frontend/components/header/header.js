1f72f801fe67d9bc3b64a8215affb14446a336b3
acidcrash2005 <acidcrash2005@gmail.com>
2018-04-18 15:40:57 +0300

    Header design (#29)
    * New header
    
    * New header + gamburger

--- frontend/components/header/header.js
+++ frontend/components/header/header.js
@@ -1,1 +1,9 @@
 import "./header.scss";
+
+$('.header-top_phones .dropdown-link').click(function () {
+
+    if ( $(this).parent().hasClass('open') ){
+        document.location.href = $(this).attr('href');
+    }
+
+});
--- frontend/components/homepage/working/working.js
+++ frontend/components/homepage/working/working.js
@@ -4,15 +4,15 @@ import "./working.css";
 /*************************
  Accordion
  *************************/
-var $acpanel = $(".iq-accordion .ad-block > .ad-details"),
-    $acsnav = $(".iq-accordion .ad-block > .ad-title");
+var $acpanel = $(".iq-accordion .tab-block > .tab-details"),
+    $acsnav = $(".iq-accordion .tab-block > .tab-title");
 
 $acpanel.hide().first().slideDown("easeOutExpo");
-$acsnav.first().addClass("ad-active");
+$acsnav.first().addClass("tab-active");
 $acsnav.on('click', function () {
-    var $this = $(this).next(".ad-details");
-    $acsnav.parent().removeClass("ad-active");
-    $(this).parent().addClass("ad-active");
+    var $this = $(this).next(".tab-details");
+    $acsnav.parent().removeClass("tab-active");
+    $(this).parent().addClass("tab-active");
     $acpanel.not($this).slideUp("easeInExpo");
     $(this).next().slideDown("easeOutExpo");
     return false;
